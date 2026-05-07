9c45662a22ba03a187e8c9b36ed509c2cdc33f6a
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2026-05-07 20:25:08 +0800

    feat: 支持下载作品,菜单栏新增部分无用菜单

--- src/components/AppBarMenu.vue
+++ src/components/AppBarMenu.vue
@@ -3,7 +3,7 @@ import { ref, inject } from "vue";
 import type { Dialog } from 'mdui/components/dialog.js';
 
 import { useBNStateStore } from "@/stores/bnState";
-
+import { downloadString } from "@/utils/blobTools";
 
 const bnState = useBNStateStore()
 const aboutDialog = inject('aboutDialog', ref<Dialog | null>(null))
@@ -45,6 +45,45 @@ const openFile = () => {
   }
 }
 
+
+const saveFile = async () => {
+  const iframeWin: any = bnState.iframeRef?.contentWindow;
+  if (!iframeWin || !iframeWin._dsaf) {
+    console.error("iframe未加载完成或不同域");
+    return;
+  }
+  let blocksInfo = { "": '' }
+  let workResult = {
+    "xml": { "": '' }, "block_count": 0, 'block_count_visible_only': 0, 'variable_dict': { '': {} }, 'broadcast_dict': {}, 'split_options': {}, 'procedure_dict': {}, "toolbox": {
+      "devices": []
+    }
+  }
+  iframeWin._dsaf.postMessageAsyn(
+    "REQUEST_ALL_SAVE_DATA",
+    {},
+    (result: any) => {
+      workResult = result
+      blocksInfo = workResult.xml
+    }
+  );
+  const actors_dict = bnState.bcmJson.actors.actors_dict
+  const scenes_dict = bnState.bcmJson.scenes.scenes_dict
+  for (const blockName of Object.keys(blocksInfo)) {
+    if (Object.keys(actors_dict).includes(blockName)) {
+      actors_dict[blockName as keyof typeof actors_dict].blocksXML = blocksInfo[blockName as keyof typeof blocksInfo]
+    } else if (Object.keys(scenes_dict).includes(blockName)) {
+      scenes_dict[blockName as keyof typeof scenes_dict].blocksXML = blocksInfo[blockName as keyof typeof blocksInfo]
+    }
+  }
+  bnState.bcmJson.block_count.all_block_count = workResult.block_count
+  bnState.bcmJson.block_count.visible_block_count = workResult.block_count_visible_only
+  bnState.bcmJson.toolbox = workResult.toolbox
+  bnState.bcmJson.variable.variable_dict = workResult.variable_dict as any
+  bnState.bcmJson.broadcast.broadcast_dict = workResult.broadcast_dict as any
+  bnState.bcmJson.procedures.procedure_dict = workResult.procedure_dict as any
+  bnState.bcmJson.split_options.options_dict = workResult.split_options as any
+  downloadString(JSON.stringify(bnState.bcmJson, null, 4), `${bnState.bcmJson.project_name}.json`)
+}
 </script>
 <template>
   <div class="top-app-bar-menu">
@@ -55,10 +94,24 @@ const openFile = () => {
         <mdui-menu-item>打开作品</mdui-menu-item>
         <mdui-divider></mdui-divider>
         <mdui-menu-item @click="openFileWindow()">打开本地作品</mdui-menu-item>
+        <mdui-menu-item @click="saveFile()">下载作品</mdui-menu-item>
       </mdui-menu>
     </mdui-dropdown>
     <mdui-dropdown>
-      <mdui-button variant="outlined" slot="trigger" class="pc-menu-button">关于</mdui-button>
+      <mdui-button variant="outlined" slot="trigger" class="pc-menu-button">作品</mdui-button>
+      <mdui-menu>
+        <mdui-menu-item>重命名</mdui-menu-item>
+        <mdui-menu-item>高级</mdui-menu-item>
+      </mdui-menu>
+    </mdui-dropdown>
+    <mdui-dropdown>
+      <mdui-button variant="outlined" slot="trigger" class="pc-menu-button">账号</mdui-button>
+      <mdui-menu>
+        <mdui-menu-item>登录</mdui-menu-item>
+      </mdui-menu>
+    </mdui-dropdown>
+    <mdui-dropdown>
+      <mdui-button variant="outlined" slot="trigger" class="pc-menu-button">帮助</mdui-button>
       <mdui-menu>
         <mdui-menu-item @click="openAbout()">关于 BetterNemo-Online</mdui-menu-item>
       </mdui-menu>
---
+++ src/utils/blobTools.ts
@@ -0,0 +1,15 @@
+export function downloadString(content: string, fileName: string) {
+  const blob = new Blob([content], { type: 'text/plain;charset=utf-8' })
+
+  const downloadUrl = URL.createObjectURL(blob)
+
+  const a = document.createElement('a')
+  a.href = downloadUrl
+  a.download = fileName
+
+  document.body.appendChild(a)
+  a.click()
+
+  document.body.removeChild(a)
+  URL.revokeObjectURL(downloadUrl)
+}
